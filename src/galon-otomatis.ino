c11039eebba1da17893508e19b283dfaac12dcbd
Fauzan Pradana <72968983+fauzanpr@users.noreply.github.com>
2021-11-02 11:46:41 +0700

    Add files via upload

---
+++ src/galon-otomatis.ino
@@ -0,0 +1,42 @@
+int trigPin = 7;
+int echoPin = 6;
+int sounder = 11;
+int tesPin = 10;
+int led = 13;
+bool gelas;
+
+void setup() {
+  Serial.begin (9600);
+  pinMode (trigPin, OUTPUT);
+  pinMode (echoPin, INPUT);
+  pinMode (sounder, OUTPUT);
+  pinMode (tesPin, INPUT);
+  pinMode (led, OUTPUT);
+}
+
+void loop() {
+  gelas = digitalRead(tesPin);
+  if (gelas == 1) {
+    digitalWrite (led, HIGH);
+    int duration, distance;
+    digitalWrite (trigPin, HIGH);
+    delayMicroseconds (10);
+    digitalWrite (trigPin, LOW);
+    duration = pulseIn (echoPin, HIGH);
+    distance = (duration / 2) / 29.1;
+    Serial.print ("Jarak Objek = ");
+    Serial.print (distance);
+    Serial.print (" cm");
+    Serial.print ("\r");
+    if (distance < 200) {
+      digitalWrite (sounder, HIGH);
+    } else {
+      digitalWrite (sounder, LOW);
+    }
+  } else {
+    digitalWrite (led, LOW);
+    digitalWrite (sounder, LOW);
+    Serial.println ("Gelas tidak ada");
+  }
+  delay (1000);
+}
